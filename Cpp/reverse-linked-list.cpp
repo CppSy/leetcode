bb82253cfd56a38abbdbd68eb287b7bcf46c9f42
CppSy <puzyryn.pl@gmail.com>
2024-06-28 09:35:39 +0200

    cpp

---
+++ Cpp/reverse-linked-list.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* node = nullptr;
+
+        while (head != nullptr) {
+            ListNode* temp = head->next;
+            head->next = node;
+            node = head;
+            head = temp;
+        }
+
+        return node;        
+    }
+};
