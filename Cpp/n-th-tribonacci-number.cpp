4b69679d984a18da6a2c0b4cfe11703289126210
CppSy <puzyryn.pl@gmail.com>
2024-07-07 14:16:14 +0200

    cpp

--- Cpp/n-th-tribonacci-number.cpp
+++ Cpp/n-th-tribonacci-number.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
 
+    int dp[40];
+
     int recg(int n) {
         if (n <= 1) // base case
             return n;
         if (n == 2) // base case
             return 1;
 
+        if (dp[n] != -1) // dp
+            return dp[n];
 
-        return recg(n - 1) + recg(n - 2) + recg(n - 3);
+        return dp[n] = 
+                recg(n - 1) + recg(n - 2) + recg(n - 3);
     }
     int tribonacci(int n) {
+
+        memset(dp, -1, sizeof(dp));
 
         return recg(n);
 
